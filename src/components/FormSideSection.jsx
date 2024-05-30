3da87c685cbd1521f3791cc0031cfe50a5f23bdf
hamdysaad20 <hamdysaad.biz@gmail.com>
2024-05-30 16:46:44 +0300

    contact and bootcamp Pages

--- src/components/FormSideSection.jsx
+++ src/components/FormSideSection.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-import Lottie from "lottie-react";
-import hand from "../public/JSON/68978-3d-hand.json";
+import hand from "../../public/JSON/68978-3d-hand.json";
+import dynamic from 'next/dynamic';
 
+const Lottie = dynamic(() => import('react-lottie'), { ssr: false });
 function FormSideSection() {
   return (
     <div className="lg:w-[500px] relative  h-full flex justify-center mx-auto min-w-[300px]">
       <div className="w-[200px] hover:scale-90 duration-1000 lg:mb-[100px] lg:w-[500px]">
-        {/* <div className='w-full flex justify-center mx-auto text-xl font-bold  text-green-400 bg-green-100 py-3 rounded-xl'>
-          (HR) is Done{"😄"}
-        </div> */}
-        <Lottie animationData={hand} />{" "}
+        <Lottie
+          options={{
+            loop: true,
+            autoplay: true,
+            animationData: hand,
+            rendererSettings: {
+              preserveAspectRatio: "xMidYMid slice",
+            },
+          }}
+          height={400}
+          width={400}
+        />
         <div>
           <h1
             className="text-2xl font-bold  text-center w-full hidden lg:block"
